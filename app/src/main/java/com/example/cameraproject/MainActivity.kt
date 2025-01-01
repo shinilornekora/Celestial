ae5f86e8595970df34be886b5624e5d2539fb6ab
Shiniasse <arokenrol@mail.ru>
2025-01-01 18:31:29 +0300

    [INITIAL]: create the project and base files

---
+++ app/src/main/java/com/example/cameraproject/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.cameraproject
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        // TODO: разобраться что это такое
+        enableEdgeToEdge()
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
---
+++ app/src/main/java/com/example/cameraproject/VideoFragment.kt
@@ -0,0 +1,28 @@
+package com.example.cameraproject
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class VideoFragment: Fragment() {
+    private fun log(message: String) {
+        Log.d("[VIDEO]", message);
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        log("onCreateView callback")
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        log("onViewCreated callback")
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
