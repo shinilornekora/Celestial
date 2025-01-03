927fa658e73033451af11563586c2e7ab2a1725a
Shiniasse <arokenrol@mail.ru>
2025-01-04 01:24:25 +0300

    [UPDATE]: linter

--- app/src/main/java/com/example/cameraproject/VideoFragment.kt
+++ app/src/main/java/com/example/cameraproject/VideoFragment.kt
@@ -2,7 +2,6 @@ package com.example.cameraproject
 
 import CameraHelper
 import android.Manifest
-import android.app.Activity
 import android.content.pm.PackageManager
 import android.os.Bundle
 import android.util.Log
